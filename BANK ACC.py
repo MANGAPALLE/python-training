d844ee28a52067b3d673a47bd3e3082a50d02f46
Kiran <madhukiran6604@gmail.com>
2025-12-28 14:48:59 +0530

    Add files via upload

---
+++ BANK ACC.py
@@ -0,0 +1,23 @@
+class Account:
+    def __init__(self,balance,accno):
+       self.balance=balance
+       self.accno=accno
+    def debit(self,amount):
+     if self.balance>amount:
+        self.balance-=amount
+        print(f"{amount}is debited,bal is(self.getbal())")
+     else:
+        print("in sufficient funds")
+    def credit(self,amount):
+        self.balance+=amount
+        print(f"{amount}is created,bal is(self.getbal())")
+    def getbal(self):
+        return self.balance
+acc1=Account(100,"acc 123")
+acc1.credit(500)
+acc1.debit(1000)
+    
+    
+   
+
+
